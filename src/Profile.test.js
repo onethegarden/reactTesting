644a0c7b87e78a17ca500c92d5c0cb86dcd5c7e9
onethegarden <onethegarden@gmail.com>
2021-08-10 22:20:59 +0900

    [react-testing-library] snapshot testing

--- src/Profile.test.js
+++ src/Profile.test.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 import Profile from "./Profile";
 
-describe("<Profile/>", () => {
+describe("<Profile />", () => {
   it("matches snapshot", () => {
-    const wrapper = mount(<Profile username="jeongwonHan" name="한정원" />);
-    expect(wrapper).toMatchSnapshot();
+    const utils = render(<Profile username="jeongwonHan" name="한정원" />);
+    expect(utils.container).toMatchSnapshot();
   });
-  it("renders username and name", () => {
-    const wrapper = mount(<Profile username="jeongwonHan" name="한정원" />);
-    expect(wrapper.props().username).toBe("jeongwonHan");
-    expect(wrapper.props().name).toBe("한정원");
-
-    const boldElement = wrapper.find("b");
-    expect(boldElement.contains("jeongwonHan")).toBe(true);
-    const spanElement = wrapper.find("span");
-    expect(spanElement.text()).toBe("한정원");
+  it("shows the props correctly", () => {
+    const utils = render(<Profile username="jeongwonHan" name="한정원" />);
+    utils.getByText("jeongwonHan");
+    utils.getByText("한정원");
+    utils.getByText(/한/);
   });
 });
