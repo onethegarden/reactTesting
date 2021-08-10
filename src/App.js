827c7c456c4fac7dfa1232ccd7df3b5cd07f3c3b
onethegarden <onethegarden@gmail.com>
2021-08-10 21:29:47 +0900

    [enzyme] 함수형 컴포넌트와 HOOK테스팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./Profile";
+import HookCounter from "./HookCounter";
 
 function App() {
   return (
     <div>
       <Profile username="jeongwonHan" name="한정원" />
+      <HookCounter />
     </div>
   );
 }
